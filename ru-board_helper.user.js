960f069e24722b61b637420c9bd1a7a7ccb79510
dartraiden <wowemuh@gmail.com>
2020-07-05 22:48:47 +0300

    Add HTTPS support

--- ru-board_helper.user.js
+++ ru-board_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Ru-Board Helper
 // @version       2.7 (11Feb10 06:01:07)
-// @include       http://forum.ru-board.com/*
+// @include       http*://forum.ru-board.com/*
 // ==/UserScript==
 
 (function() {
